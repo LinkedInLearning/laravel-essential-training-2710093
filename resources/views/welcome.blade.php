13115a1782213387d45bffa834fbf4812611649e
Shruti Balasa <69137210+shrutibalasawebdev@users.noreply.github.com>
2024-06-06 05:42:49 +0000

    Handling routing ends

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -111,7 +111,7 @@
                                 </div>
 
                                 <div class="pt-3 sm:pt-5">
-                                    <h2 class="text-xl font-semibold text-black dark:text-white">Laracasts</h2>
+                                    <h2 class="text-xl font-semibold text-black dark:text-white">Laracasts Videos</h2>
 
                                     <p class="mt-4 text-sm/relaxed">
                                         Laracasts offers thousands of video tutorials on Laravel, PHP, and JavaScript development. Check them out, see for yourself, and massively level up your development skills in the process.
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/home', function () {
     return view('welcome');
 });
