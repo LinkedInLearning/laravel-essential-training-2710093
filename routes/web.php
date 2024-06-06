5aa1fa1abd74be264468da8b59832d3b45b91c16
Shruti Balasa <69137210+shrutibalasawebdev@users.noreply.github.com>
2024-06-06 05:58:04 +0000

    Creating a view ends

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return "<h1>Welcome</h1>";
+});
 
 Route::get('/home', function () {
-    return view('welcome');
+    return view('home', ['name' => "Shruti"]);
 });
