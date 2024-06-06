f18667a72077eaa6e9deddec29abe522076611b2
Shruti Balasa <69137210+shrutibalasawebdev@users.noreply.github.com>
2024-06-06 06:48:02 +0000

    Understand Laravel configuration

--- routes/web.php
+++ routes/web.php
@@ -4,14 +4,6 @@ use App\Http\Controllers\GreetingController;
 use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [WelcomeController::class, 'index']);
-
-Route::get('/home', function () {
-    return view('home');
+Route::get('/', function(){
+    return view('welcome');
 });
-
-Route::get('/about', function () {
-    return view('about');
-});
-
-Route::get('/hello', [GreetingController::class, 'index']);
