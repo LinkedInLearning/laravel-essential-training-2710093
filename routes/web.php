19e47c3739350311d4d1c09b6750bdf67f816079
Shruti Balasa <69137210+shrutibalasawebdev@users.noreply.github.com>
2024-06-06 06:48:15 +0000

    Understand Laravel configuration

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,7 @@
 <?php
 
-use App\Http\Controllers\GreetingController;
-use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function(){
+Route::get('/', function () {
     return view('welcome');
 });
