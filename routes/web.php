75f232cb0ae277ae441bbcbdcb29100dceea71f1
Shruti Balasa <69137210+shrutibalasawebdev@users.noreply.github.com>
2024-06-07 05:28:16 +0000

    Working with the database

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    // 1. Using raw SQL queries
+    // $users = DB::select('select * from users');
+    // dd($users);
+
+    // 2. Query builder
+    $users = DB::table('users')->select(['name', 'email'])->whereNotNull('email')->orderBy('name')->get();
+    dd($users);
+
+    // 3. Eloquent ORM
 });
