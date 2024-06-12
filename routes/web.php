59de3205c706b71ae8d93d788ed7d7501c9c056e
Shruti Balasa <69137210+shrutibalasawebdev@users.noreply.github.com>
2024-06-12 05:12:56 +0000

    Spelling corrected

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    // Get the student model where email is john@exmaple.com
+    // Get the student model where email is john@example.com
 
     // Update the 'status' column with the value 'active'
 
