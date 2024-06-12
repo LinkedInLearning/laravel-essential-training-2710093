ba3f8efaefd70aec432179c9822841252326d8c6
Shruti Balasa <69137210+shrutibalasawebdev@users.noreply.github.com>
2024-06-12 05:45:41 +0000

    Solution to add new column

--- routes/web.php
+++ routes/web.php
@@ -6,11 +6,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     // Get the student model where email is john@example.com
+    $student = Student::where('email', 'john@example.com')->first();
 
     // Update the 'status' column with the value 'active'
+    $student->status = 'active';
 
     // Save the model
+    $student->save();
 
     // Dump the result
-
+    dd($student);
 });
