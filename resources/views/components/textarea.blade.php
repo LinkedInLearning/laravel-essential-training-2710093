62111297e159c04eca5b623b00ae208caacc6850
Shruti Balasa <69137210+shrutibalasawebdev@users.noreply.github.com>
2024-06-14 07:17:46 +0000

    Minor change

--- resources/views/components/textarea.blade.php
+++ resources/views/components/textarea.blade.php
@@ -1,3 +1,3 @@
 @props(['disabled' => false, 'value' => ''])
 
-<textarea {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm']) !!}>{{ $value }}</textarea>
+<textarea {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm']) !!}>{{ htmlspecialchars_decode($value) }}</textarea>
