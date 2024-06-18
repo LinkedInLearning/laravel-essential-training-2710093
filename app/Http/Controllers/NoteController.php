c35a2cfa08f589d4c20b15f4b54b5a3df5543caa
Shruti Balasa <69137210+shrutibalasawebdev@users.noreply.github.com>
2024-06-18 06:27:33 +0000

    Solution: Update notes with new resource

--- app/Http/Controllers/NoteController.php
+++ app/Http/Controllers/NoteController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Note;
+use App\Models\Notebook;
 use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -24,7 +25,8 @@ class NoteController extends Controller
      */
     public function create()
     {
-        return view('notes.create');
+        $notebooks = Notebook::where('user_id', Auth::id())->get();
+        return view('notes.create')->with('notebooks', $notebooks);
     }
 
     /**
@@ -41,8 +43,10 @@ class NoteController extends Controller
             'user_id' => Auth::id(),
             'uuid' => Str::uuid(),
             'title' => $request->title,
-            'text' => $request->text
+            'text' => $request->text,
+            'notebook_id' => $request->notebook_id
         ]);
+
         $note->save();
 
         return to_route('notes.show', $note);
@@ -69,7 +73,9 @@ class NoteController extends Controller
             abort(403);
         }
 
-        return view('notes.edit', ['note' => $note]);
+        $notebooks = Notebook::where('user_id', Auth::id())->get();
+
+        return view('notes.edit', ['note' => $note, 'notebooks' => $notebooks]);
     }
 
     /**
@@ -88,7 +94,8 @@ class NoteController extends Controller
 
         $note->update([
             'title' => $request->title,
-            'text' => $request->text
+            'text' => $request->text,
+            'notebook_id' => $request->notebook_id
         ]);
 
         return to_route('notes.show', $note)
--- resources/views/notes/create.blade.php
+++ resources/views/notes/create.blade.php
@@ -10,14 +10,24 @@
             <div class="bg-white p-6 overflow-hidden shadow-sm sm:rounded-lg max-w-2xl">
               <form action="{{ route('notes.store') }}" method="post">
                 @csrf
+                
                 <x-text-input name="title" class="w-full" placeholder="Note title" value="{{ @old('title') }}"></x-text-input>
                 @error('title')
                     <div class="text-sm mt-1 text-red-500">{{ $message }}</div>
                 @enderror
+                
                 <x-textarea name="text" placeholder="Type your note" rows="8" value="{{ @old('text') }}" class="w-full mt-6"></x-textarea>
                 @error('text')
                     <div class="text-sm mt-1 text-red-500">{{ $message }}</div>
                 @enderror
+
+                <select name="notebook_id" class="w-full mt-6 border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm">
+                    <option value="">-- Select Notebook --</option>
+                    @foreach ($notebooks as $notebook)
+                        <option value="{{ $notebook->id }}">{{ $notebook->name }}</option>
+                    @endforeach
+                </select>
+                
                 <x-primary-button class="mt-6">Save note</x-primary-button>
               </form>
             </div>
