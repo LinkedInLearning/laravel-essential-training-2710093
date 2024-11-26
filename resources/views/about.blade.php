a0489ee0cc6f7bf217fcdd75b38f1f67cbf80964
Oludayo Adeoye <49393855+dbillion@users.noreply.github.com>
2024-11-26 18:54:00 +0000

    ui

---
+++ resources/views/about.blade.php
@@ -0,0 +1,7 @@
+@extends('layout.base')
+
+@section('name','oludayo')
+
+@section('content')
+<h1 class="text-4xl font-bold"> About us</h1>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,20 @@
 <?php
 
+use App\Http\Controllers\GreetingController;
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return "<h1>Welcome</h1>";
-});
+// Route::get('/', [WelcomeController::class, 'index']);
+Route::get('/hello', [GreetingController::class, 'index']);
 
 Route::get('/home', function () {
-    return view('home', ['name' => "Shruti"]);
+    return view('home', ['name' => "dayo"]);
+});
+
+Route::get('/about', function () {
+    return view('about', ['name' => "dayo"]);
+});
+
+Route::get('/', function () {
+    return view('welcome');
 });
